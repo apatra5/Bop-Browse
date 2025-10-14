16e8d0aee126f3763f78d2b5b25b4a7045ac97e5
Ivy Zhu <ivy.hanzhang.zhu@gmail.com>
2025-10-14 15:47:08 -0500

    removed type column from category table

--- backend/models/category.py
+++ backend/models/category.py
@@ -5,10 +5,18 @@ from .associations import item_category
 
 
 class Category(Base):
+    """"
+    Category model representing item categories.
+    Attributes:
+        id (int): Primary key.
+        name (str): Name of the category.
+        itemCount (int): Number of items in this category.
+
+        items (List[Item]): List of items associated with this category.
+    """
     __tablename__ = "categories"
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
-    type = Column(String)
     itemCount = Column(Integer)
 
     items = relationship("Item", secondary=item_category, back_populates="categories")
