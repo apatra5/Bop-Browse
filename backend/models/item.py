c31cc0b0462f6dc9804ad5694390c74cf2a7735e
Ivy Zhu <ivy.hanzhang.zhu@gmail.com>
2025-10-26 17:33:58 -0500

    Added helper functions in item model and item crud

--- backend/models/item.py
+++ backend/models/item.py
@@ -11,3 +11,6 @@ class Item(Base):
 
     categories = relationship("Category", secondary=item_category, back_populates="items")
     outfits = relationship("Outfit", secondary=item_outfit, back_populates="items")
+
+    def __repr__(self):
+        return f"<Item(id={self.id}, name={self.name})>"
