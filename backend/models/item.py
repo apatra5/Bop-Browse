bb18d04b7ffc67509908aa7a7e24990b6389e2e1
Ivy Zhu <ivy.hanzhang.zhu@gmail.com>
2025-10-27 15:53:43 -0500

    Added embedding column to the item table

--- backend/models/item.py
+++ backend/models/item.py
@@ -1,5 +1,7 @@
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from pgvector.sqlalchemy import Vector
+
 from db.base import Base
 from .associations import item_category, item_outfit
 
@@ -8,6 +10,7 @@ class Item(Base):
     id = Column(String, primary_key=True, index=True)
     name = Column(String)
     image_url_suffix = Column(String, nullable=True)
+    embedding = Column(Vector(768), nullable=True)
 
     categories = relationship("Category", secondary=item_category, back_populates="items")
     outfits = relationship("Outfit", secondary=item_outfit, back_populates="items")
