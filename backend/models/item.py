54d58a7ffb6ff13d73606fd6b0ad79e7b0cd7fa0
Ivy Zhu <ivy.hanzhang.zhu@gmail.com>
2025-10-27 16:52:49 -0500

    Fixed liked_item and disliked_item relationships

--- backend/models/item.py
+++ backend/models/item.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import relationship
 from pgvector.sqlalchemy import Vector
 
 from db.base import Base
-from .associations import item_category, item_outfit
+from .associations import item_category, item_outfit, user_dislike_items, user_like_items
 
 class Item(Base):
     __tablename__ = "items"
@@ -14,6 +14,10 @@ class Item(Base):
 
     categories = relationship("Category", secondary=item_category, back_populates="items")
     outfits = relationship("Outfit", secondary=item_outfit, back_populates="items")
+
+    liked_by_users = relationship("User", secondary=user_like_items, back_populates="liked_items")
+    disliked_by_users = relationship("User", secondary=user_dislike_items, back_populates="disliked_items")
+
 
     def __repr__(self):
         return f"<Item(id={self.id}, name={self.name})>"
